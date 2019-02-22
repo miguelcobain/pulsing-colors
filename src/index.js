b96a71fd48db97413fa24b6e1ebd0ddc9a297c43
Miguel Andrade <jmandrade0@gmail.com>
2019-02-22 00:50:36 +0000

    add service worker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+// Activate the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    let swPath = 'service-worker.js';
+    navigator.serviceWorker.register(swPath).then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 import './styles.scss';
 import screenfull from 'screenfull';
 
